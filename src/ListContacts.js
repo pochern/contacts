eadf0adb1b5e2844239208591d8e91e99654f899
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-10 18:08:01 -0500

    Replaced anchor tag with Link component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import { Link } from 'react-router-dom'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -45,11 +46,10 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <a
-            href='#create'
-            onClick={this.props.onNavigate}
+          <Link
+            to='/create'
             className='add-contact'
-          >Add contact</a>
+          >Add contact</Link>
         </div>
 
         { showingContacts.length !== contacts.length && (
