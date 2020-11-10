cdbd7d4dd135a97d7d5da658136001a43e4c6170
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-10 18:23:25 -0500

    Added Route to render different screens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [],
-    screen: 'list' // to switch between list and create
+    contacts: []
   }
 
   componentDidMount () {
@@ -26,18 +26,13 @@ class App extends Component {
   render () {
     return (
       <div>
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
-            onNavigate={() => {
-              this.setState({ screen: 'create' })
-            }}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
+        )} />
+        <Route path='/create' component={CreateContact} />
       </div>
     )
   }
