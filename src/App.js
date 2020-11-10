32d6ef36076898ebf19bce27fb428906f5d817bf
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-10 10:21:07 -0500

    Changing state of screen to create contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: 'create' // to switch between list and create
   }
 
   componentDidMount () {
@@ -24,7 +26,12 @@ class App extends Component {
   render () {
     return (
       <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+        {this.state.screen === 'list' && (
+          <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact />
+        )}
       </div>
     )
   }
